18cdcc912d56eb49a5721c7d4b8c42c96851ae2f
Obaid Salikeen <obaid.salikeen@ask.com>
2015-01-04 18:37:20 -0800

    Docker images and Marathon recipes

---
+++ docker/storm-nimbus/configure-storm.sh
@@ -0,0 +1,7 @@
+sed -i -e "s/%zookeeper%/$MASTER_HOSTNAME/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$MASTER_HOSTNAME/g" $STORM_HOME/conf/storm.yaml
+
+echo "storm.local.hostname: $MASTER_HOSTNAME" >> $STORM_HOME/conf/storm.yaml
+echo "storm.local.dir: /ask/storm/local_dir" >> $STORM_HOME/conf/storm.yaml
+
+/usr/bin/storm nimbus
---
+++ docker/storm-supervisor/configure-storm.sh
@@ -0,0 +1,6 @@
+sed -i -e "s/%zookeeper%/$MASTER_HOSTNAME/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/%nimbus%/$MASTER_HOSTNAME/g" $STORM_HOME/conf/storm.yaml
+echo "storm.local.hostname: $MASTER_HOSTNAME" >> $STORM_HOME/conf/storm.yaml
+echo "storm.local.dir: /ask/storm/local_dir" >> $STORM_HOME/conf/storm.yaml
+
+/usr/bin/storm supervisor
